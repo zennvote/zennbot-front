7cd8bf36863bea9dc8b2fdd62ea88c44461e5fe5
wArrnp <gtobio11@gmail.com>
2021-10-13 23:24:12 +0900

    image, style load setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import './styles.css';
+import IMG from './sample.png';
+import CIRCLE from './circle.svg';
+
 export const App = () => {
-    return <div>init project</div>
+    return <>
+        <h1>init project</h1>
+        <img src={IMG} alt="test Img" width="500" height ="500"/>
+        <img src={CIRCLE} alt="test Circle" width="500" height="500" />
+    </>
 }
