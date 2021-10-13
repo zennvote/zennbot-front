57acd9e6ef49570843190b621a3d3c23d75ee930
wArrnp <gtobio11@gmail.com>
2021-10-13 23:43:12 +0900

    webpack config 분리

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CIRCLE from './circle.svg';
 
 export const App = () => {
     return <>
-        <h1>init project</h1>
+        <h1>init project  - {process.env.NODE_ENV}</h1>
         <img src={IMG} alt="test Img" width="500" height ="500"/>
         <img src={CIRCLE} alt="test Circle" width="500" height="500" />
     </>
