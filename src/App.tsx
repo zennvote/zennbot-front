9245e2edf6cbbe60d3134330bdfa6758137a9976
wArrnp <gtobio11@gmail.com>
2021-10-18 23:31:16 +0900

    run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import IMG from './sample.png';
 import CIRCLE from './circle.svg';
 
 export const App = () => {
-    return <>
-        <h1>init project  - {process.env.NODE_ENV}</h1>
-        <img src={IMG} alt="test Img" width="500" height ="500"/>
-        <img src={CIRCLE} alt="test Circle" width="500" height="500" />
-    </>
-}
+    return (
+        <>
+            <h1>init project - {process.env.NODE_ENV}</h1>
+            <img src={IMG} alt="test Img" width="500" height="500" />
+            <img src={CIRCLE} alt="test Circle" width="500" height="500" />
+        </>
+    );
+};
