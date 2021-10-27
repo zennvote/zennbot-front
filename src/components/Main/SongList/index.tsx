1311aef40915ed77a65f3a5a98b97b3d8d9d63f1
wArrnp <gtobio11@gmail.com>
2021-10-27 23:41:39 +0900

    [#3] 신청곡 목록에서 한 아이템에 대한 골자 구성

--- src/components/Main/SongList/index.tsx
+++ src/components/Main/SongList/index.tsx
@@ -8,10 +8,15 @@ interface SongListProps {
 
 const SongList: React.FC<SongListProps> = ({ songList }) => {
     return (
-        <div>
+        <div className="main-song_list-wrapper">
             {songList.map((song, idx) => (
                 // TODO: idx가 아닌 song의 id 값이 존재하다면, 그 값을 사용해서 key 설정해주어야 함.
-                <div key={idx}>{song.title}</div>
+                <div className="main-song_list-item" key={idx}>
+                    <p className="main-song_list-item-title">{song.title}</p>
+                    <p className="main-song_list-item-requestor_name">
+                        {song.requestorName}
+                    </p>
+                </div>
             ))}
         </div>
     );
