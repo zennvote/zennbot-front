71093e55bc68f8f0713ecbc9eebe708888901d2c
wArrnp <gtobio11@gmail.com>
2021-10-28 22:56:48 +0900

    [#3] song item Component로 분리

--- src/components/Main/SongList/index.tsx
+++ src/components/Main/SongList/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SongType from '../../../types/Song';
+import SongItem from '../SongItem';
 import './index.scss';
 
 interface SongListProps {
@@ -11,12 +12,7 @@ const SongList: React.FC<SongListProps> = ({ songList }) => {
         <div className="main-song_list-wrapper">
             {songList.map((song, idx) => (
                 // TODO: idx가 아닌 song의 id 값이 존재하다면, 그 값을 사용해서 key 설정해주어야 함.
-                <div className="main-song_list-item" key={idx}>
-                    <p className="main-song_list-item-title">{song.title}</p>
-                    <p className="main-song_list-item-requestor_name">
-                        {song.requestorName}
-                    </p>
-                </div>
+                <SongItem key={idx} songItem={song} />
             ))}
         </div>
     );
