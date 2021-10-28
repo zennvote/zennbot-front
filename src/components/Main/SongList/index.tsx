2d6b589795e49d0e119f438da7d88091149e1ed4
wArrnp <gtobio11@gmail.com>
2021-10-28 23:18:35 +0900

    [#3] 신청곡 목록 타이틀과 다음 곡 버튼 추가
    - Style 아직 안붙힘.

--- src/components/Main/SongList/index.tsx
+++ src/components/Main/SongList/index.tsx
@@ -10,10 +10,16 @@ interface SongListProps {
 const SongList: React.FC<SongListProps> = ({ songList }) => {
     return (
         <div className="main-song_list-wrapper">
-            {songList.map((song, idx) => (
-                // TODO: idx가 아닌 song의 id 값이 존재하다면, 그 값을 사용해서 key 설정해주어야 함.
-                <SongItem key={idx} songItem={song} />
-            ))}
+            <div className="main-song_list-header">
+                <span>신청곡 목록</span>
+                <button>다음 곡</button>
+            </div>
+            <div className="main-song_list-content-wrapper">
+                {songList.map((song, idx) => (
+                    // TODO: idx가 아닌 song의 id 값이 존재하다면, 그 값을 사용해서 key 설정해주어야 함.
+                    <SongItem key={idx} songItem={song} />
+                ))}
+            </div>
         </div>
     );
 };
