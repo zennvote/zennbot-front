4da07338a14afcd28899405bd34a5ed64f67556d
wArrnp <gtobio11@gmail.com>
2021-11-11 21:52:48 +0900

    [#11] settings component 파일 생성

--- src/components/Main/Content/index.tsx
+++ src/components/Main/Content/index.tsx
@@ -1,10 +1,13 @@
+import Settings from '../Settings';
 import SongList from '../SongList';
 import './index.scss';
 
 const Content = () => {
     return (
         <div className="main-content-wrapper">
-            <div className="main-content-left">left</div>
+            <div className="main-content-left">
+                <Settings />
+            </div>
             <div className="main-content-right">
                 <SongList />
             </div>
---
+++ src/components/Main/Settings/index.tsx
@@ -0,0 +1,7 @@
+import './index.scss'
+
+const Settings = () => {
+    return <div className="main-content-settings-wrapper">Settings</div>;
+};
+
+export default Settings;
