01949d3ecf33d828815ef6f28935b1cd6b95f57a
wArrnp <gtobio11@gmail.com>
2021-11-15 22:06:04 +0900

    [#11] 콘텐츠 메인에 헤더 추가

--- src/components/Main/Settings/index.tsx
+++ src/components/Main/Settings/index.tsx
@@ -1,7 +1,11 @@
-import './index.scss'
+import './index.scss';
 
 const Settings = () => {
-    return <div className="main-content-settings-wrapper">Settings</div>;
+    return (
+        <div className="main-content-settings-wrapper">
+            <h1 className="main-content-settings-header">환경 설정</h1>
+        </div>
+    );
 };
 
 export default Settings;
