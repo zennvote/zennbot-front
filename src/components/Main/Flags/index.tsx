1c1194089b0845d63333f35f5b016bc0a2e27195
wArrnp <gtobio11@gmail.com>
2021-11-16 21:50:35 +0900

    [#11] flag item 컴포넌트를 flags에 연결
    - TODO: API 연결
    - TODO: 신청곡 관련 테스트
    - TODO: Style

--- src/components/Main/Flags/index.tsx
+++ src/components/Main/Flags/index.tsx
@@ -1,9 +1,32 @@
+import { useCallback, useEffect, useState } from 'react';
+import FlagItem from '../FlagItem';
+
 import './index.scss';
 
 const Flags = () => {
+    const [freemode, setFreemode] = useState<boolean>(false);
+
+    useEffect(() => {
+        return () => undefined;
+    }, []);
+
+    const handleToggleFreemode = useCallback(() => {
+        setFreemode(!freemode);
+    }, [freemode]);
+
     return (
         <div className="main-content-settings-flag-wrapper">
             <h2 className="main-content-settings-flag-header">플래그 관리</h2>
+            <FlagItem
+                title="신청곡 활성화"
+                value={false}
+                handleClickToggle={() => undefined}
+            />
+            <FlagItem
+                title="골든벨 활성화 (무료 모드)"
+                value={freemode}
+                handleClickToggle={handleToggleFreemode}
+            />
         </div>
     );
 };
