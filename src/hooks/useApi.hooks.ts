5950c557fff45fb2bc324b43b76ba8436b4ab811
wArrnp <gtobio11@gmail.com>
2021-11-17 22:16:36 +0900

    [#11] freemode change api 호출용 hook 추가

--- src/hooks/useApi.hooks.ts
+++ src/hooks/useApi.hooks.ts
@@ -1,9 +1,19 @@
 import { useCallback } from 'react';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export const usePostNextSong = () => {
     return useCallback(() => {
         // TODO: https로 변경되면, https 반영해야함.
         return axios.post('http://zennbot.net/api/songs/next');
+    }, []);
+};
+
+export const useSetFreemodeHook = (): ((
+    freemode: boolean
+) => Promise<AxiosResponse>) => {
+    return useCallback((freemode: boolean) => {
+        return axios.post('http://zennbot.net/api/flags/freemode', {
+            value: freemode,
+        });
     }, []);
 };
