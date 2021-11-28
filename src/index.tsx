4e94a390ff92c51986d27bf506e2fdabe5904031
wArrnp <gtobio11@gmail.com>
2021-11-28 21:30:45 +0900

    [#11] GlobalStyle 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,12 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import GlobalStyle from './assets/GlobalStyle';
+
+ReactDOM.render(
+    <>
+        <GlobalStyle />
+        <App />
+    </>,
+    document.getElementById('root')
+);
